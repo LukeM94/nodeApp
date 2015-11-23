051574b37ef14cd649c7f2da0291be409a46aafa
LukeM94 <DRU9HEde>
2015-11-23 14:58:17 +0000

    Commit

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+var http = require('http')
+var port = process.env.PORT || 1337;
+
 http.createServer(function(request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.write('Connecting \n');
@@ -21,3 +24,4 @@ http.createServer(function(request, response) {
     });
 
 }).listen(port);
+
